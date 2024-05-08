98230fb3997b0c31ead66862ba2a50b55deefbad
nhkim06 <nhkim7788@gmail.com>
2024-05-08 12:01:26 +0900

    add, sub

--- week10.py
+++ week10.py
@@ -5,8 +5,13 @@ class Complex:
     
     def __str__(self):
         return f'{self.a} + {self.b}i'
+
+    def __add__(self, other):
+        return f'{self.a + other.a} + {self.b + other.b}i'
     
-    
+    def __sub__(self, other):
+        return f'{self.a - other.a} + {self.b - other.b}i'
+
 A=Complex(1, 2)
 B=Complex(3, 4)
 print(A+B)
